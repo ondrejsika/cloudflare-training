eeb7938004a35bedda0d07f087a5dd7b340ee7fd
Ondrej Sika <ondrej@ondrejsika.com>
2025-07-14 22:09:20 +0200

    feat(scripts/cleanup): Add token cleanup

--- scripts/cleanup/main.go
+++ scripts/cleanup/main.go
@@ -4,6 +4,8 @@ package main
 // https://developers.cloudflare.com/api/go/resources/dns/subresources/records/methods/delete/
 // https://developers.cloudflare.com/api/go/resources/zero_trust/subresources/tunnels/subresources/cloudflared/methods/list/
 // https://developers.cloudflare.com/api/go/resources/zero_trust/subresources/tunnels/subresources/cloudflared/methods/delete/
+// https://developers.cloudflare.com/api/go/resources/accounts/subresources/tokens/methods/list/
+// https://developers.cloudflare.com/api/go/resources/accounts/subresources/tokens/methods/delete/
 
 import (
 	"context"
@@ -14,6 +16,7 @@ import (
 	"github.com/cloudflare/cloudflare-go/v3"
 	"github.com/cloudflare/cloudflare-go/v3/dns"
 	"github.com/cloudflare/cloudflare-go/v3/option"
+	"github.com/cloudflare/cloudflare-go/v3/user"
 	"github.com/cloudflare/cloudflare-go/v3/zero_trust"
 	"github.com/joho/godotenv"
 )
@@ -33,6 +36,9 @@ func main() {
 	zone1 := "3ba5d048f4d88833ae1e2638ad57ee64"     // sikademo1.uk zone
 	zone2 := "dd0036cc8abdf8d12eb9a8cc150d9f08"     // sikademo2.uk zone
 	zone3 := "db23d39fbe9feaf207f0007680022fbc"     // sikademo3.uk zone
+
+	err = deleteAllTokens(email, apiKey)
+	handleErrorFatal(err)
 
 	err = deleteAllTunnels(email, apiKey, accountID)
 	handleErrorFatal(err)
@@ -107,6 +113,33 @@ func deleteAllTunnels(email, apiKey, accountID string) error {
 		)
 		if err != nil {
 			return fmt.Errorf("failed to delete tunnel %s: %w", tunnel.Name, err)
+		}
+	}
+
+	return nil
+}
+
+func deleteAllTokens(email, apiKey string) error {
+	var err error
+
+	client := cloudflare.NewClient(
+		option.WithAPIEmail(email),
+		option.WithAPIKey(apiKey),
+	)
+
+	page, err := client.User.Tokens.List(context.TODO(), user.TokenListParams{})
+	if err != nil {
+		return fmt.Errorf("failed to list tokens: %w", err)
+	}
+
+	for _, token := range page.Result {
+		fmt.Println("Deleting token:", token.Name)
+		_, err = client.User.Tokens.Delete(
+			context.TODO(),
+			token.ID,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to delete token %s: %w", token.Name, err)
 		}
 	}
 
