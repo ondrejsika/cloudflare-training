a1450182760e4801eb8f2c723daf72fa7c01ca19
Ondrej Sika <ondrej@ondrejsika.com>
2025-07-15 07:11:32 +0200

    fix(terraform/dns): fix record name and ttl

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_dns_record" "hello" {
   zone_id = local.sikademo4_uk_zone_id
-  name    = "hello"
+  name    = "hello.sikademo4.uk"
   type    = "TXT"
   content = "Hello world from DNS!"
-  ttl     = 0
+  ttl     = 1
 }
